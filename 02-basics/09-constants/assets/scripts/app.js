133e04a52c582ecc3321813afa694ab4ebd582a8
benj-lazaro <benjie.work@gmail.com>
2024-03-12 15:00:21 +0800

    Section 02 constants initial commit

---
+++ 02-basics/09-constants/assets/scripts/app.js
@@ -0,0 +1,13 @@
+// Default value NEVER changes throughout the execution of the code
+const defaultResult = 0;
+
+// Copy the value of defaultResult to currentResult
+let currentResult = defaultResult;
+
+// Overwrite initial value of currentResult with the result of the mathematical expression
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+let calculationDescription = "((" + defaultResult + " + 10) * 3) / 2 - 1";
+
+// Call outputResult() function from vendor.js to render value of variables in HTML
+outputResult(currentResult, calculationDescription);
