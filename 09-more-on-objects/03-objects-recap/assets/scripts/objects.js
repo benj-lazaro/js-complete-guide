3195fc4cbe729730b5efcbd226fd071eb44ed415
benj-lazaro <benjie.work@gmail.com>
2024-05-17 18:09:51 +0800

    Section 09 objects recap initial commit

---
+++ 09-more-on-objects/03-objects-recap/assets/scripts/objects.js
@@ -0,0 +1,12 @@
+// Define an object with properties and a method
+const person = {
+  name: "Max",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  greet: function () {
+    alert("Hi there!");
+  },
+};
+
+// Call the object's method .greet()
+person.greet();
