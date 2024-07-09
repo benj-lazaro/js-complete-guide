a71e8d3cf3ccc3526b83d993681379b676a93c75
benj-lazaro <benjie.work@gmail.com>
2024-07-09 17:08:39 +0800

    Section 07 document and window content refactor

---
+++ 07-working-with-the-dom/03-document-and-window/code/app.js
@@ -0,0 +1,10 @@
+// document and window Demo
+
+// Display the content of the "document" object
+console.log(document);
+
+// Access the "document" object's READ-ONLY properties
+console.dir(document);
+
+// Display the content (properties & method) of the "window" object
+console.log(window);
