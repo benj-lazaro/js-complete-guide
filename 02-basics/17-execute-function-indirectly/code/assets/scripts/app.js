07887a3c66b197e39a2ca1fd79ac5c7ec682ea48
benj-lazaro <benjie.work@gmail.com>
2025-12-19 22:35:13 +0800

    17 execute functions indirectly

--- 02-basics/17-execute-function-indirectly/code/assets/scripts/app.js
+++ 02-basics/17-execute-function-indirectly/code/assets/scripts/app.js
@@ -1,12 +1,13 @@
+// Global scope constant(s) & variable(s)
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// A function that returns the sum of two numbers
 function add(num1, num2) {
-  // Add userInput's value with the value stored in the global variable currentResult
-  // userInput is defined in vendor.js
   currentResult = currentResult + userInput.value;
+
   outputResult(currentResult, "");
 }
 
-// Hook up an Event Listener to the targeted HTML element defined in vendor.js
+// Hook up an Event Listener tp the constant "addBtn" (defined in vendor.js)
 addBtn.addEventListener("click", add);
