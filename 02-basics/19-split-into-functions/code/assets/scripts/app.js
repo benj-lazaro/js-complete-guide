ea424f238e2607bac7cbde3ab7017c0367d9213a
benj-lazaro <benjie.work@gmail.com>
2025-12-21 17:18:12 +0800

    19 splitting code into functions

--- 02-basics/19-split-into-functions/code/assets/scripts/app.js
+++ 02-basics/19-split-into-functions/code/assets/scripts/app.js
@@ -1,16 +1,26 @@
+// Global scope constant(s) & variable(s)
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// A Function that fetch & returns the value of the HTML element <input> with an "id" of "input-number"
 function getUserNumberInput() {
   return parseInt(userInput.value);
 }
 
+// A Function that renders the mathematical expression & sum of two numbers
 function add(num1, num2) {
+  // Stores the user-input numeric value
   const enteredNumber = getUserNumberInput();
+
+  // Stores the mathematical expression using template literal strings
   const calcDescription = `${currentResult} + ${enteredNumber}`;
+
+  // Calculate the sum
   currentResult = currentResult + enteredNumber;
+
+  // Renders the mathematical expression & sum on their respective HTML element <h2>
   outputResult(currentResult, calcDescription);
 }
 
-// Hook up an Event Listener to the targeted HTML element defined in vendor.js
+// Hooks up an Event Listener for a "click" & calls the Function "add()"
 addBtn.addEventListener("click", add);
