1565b4d534af47e1ab729a0b87f2239f828185e5
benj-lazaro <benjie.work@gmail.com>
2025-12-31 17:53:17 +0800

    09 understanding the starter code

---
+++ 02-basics/09-starter-code/code/assets/scripts/app.js
@@ -0,0 +1,8 @@
+// Declare a variable
+let currentResult = 0;
+
+// Assign the result of the mathematical expression back to the variable
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+// Render the result on the HTML document
+outputResult(currentResult, "");
