3f9aed02622ea4738cd9f67cf734f4353d76cb1d
benj-lazaro <benjie.work@gmail.com>
2025-12-31 17:21:24 +0800

    07 quiz 01 variables & operators

--- 02-basics/07-understand-starter-code/code/assets/scripts/app.js
+++
@@ -1,8 +0,0 @@
-// Initialize variable currentResult
-let currentResult = 0;
-
-// Evaluate and store the result of the mathematical expression in the variable
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
-
-// Pass two argument values; the value of currentResult and a blank string
-outputResult(currentResult, "");
