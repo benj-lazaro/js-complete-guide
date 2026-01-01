a0a024d417abc039e5fb76ec7abb5790dfd0eef6
benj-lazaro <benjie.work@gmail.com>
2026-01-01 18:02:42 +0800

    19 coding exercise 02 functions

---
+++ 02-basics/19-coding-exercise-02/code/index.js
@@ -0,0 +1,11 @@
+// Todo: Your solution code goes here
+
+// Returns the difference
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+// Returns a string w/ the passed argument number value included
+function stringify(number) {
+  return `Result: ${number}`;
+}
