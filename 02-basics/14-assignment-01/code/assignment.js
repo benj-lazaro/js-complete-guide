f5e577f454e472c9e9734b0a366597f1e9a34a7b
benj-lazaro <benjie.work@gmail.com>
2026-01-01 14:15:41 +0800

    14 assignment 01 variables, operators, data types

---
+++ 02-basics/14-assignment-01/code/assignment.js
@@ -0,0 +1,13 @@
+// Declare two variables
+const userInput = 10;
+let result;
+
+// Perform the mathematical operations
+result = 18 + userInput;
+result = result - 1;
+result = result * 1;
+result = result / 1;
+
+// Output the results
+alert(userInput);
+alert(result);
