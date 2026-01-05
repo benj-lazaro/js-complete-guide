abf01fd26b0f78dbf6f207906dd95e4298ac9751
benj-lazaro <benjie.work@gmail.com>
2026-01-05 23:23:54 +0800

    45 the typeof operator

---
+++ 02-basics/45-typeof-operator/code/index.js
@@ -0,0 +1,13 @@
+console.log(typeof "Max"); // Returns the data type of string
+
+console.log(typeof 123); // Returns the data type of number
+
+console.log(typeof undefined); // Returns the data type of undefined
+
+console.log(typeof null); // Returns the data type of object
+
+console.log(typeof NaN); // Returns the data type of number
+
+console.log(typeof { name: "Max", age: 50 }); // Returns the data type of object
+
+console.log(typeof [1, 2, 3]); // Returns the data type of object
