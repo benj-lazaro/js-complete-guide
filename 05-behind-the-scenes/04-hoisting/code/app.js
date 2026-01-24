cb0a8a210725f706524ba575763ff0f872d57207
benj-lazaro <benjie.work@gmail.com>
2026-01-25 03:22:19 +0800

    04 understanding hoisting

---
+++ 05-behind-the-scenes/04-hoisting/code/app.js
@@ -0,0 +1,7 @@
+// Uses a variable prior declaration
+console.log(userName1); // ReferenceError
+console.log(userName2); // "undefined" value
+
+// Declaration
+let userName1 = "Max";
+var userName2 = "Manuel";
