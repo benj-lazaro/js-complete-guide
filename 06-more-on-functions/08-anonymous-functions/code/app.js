02646214cbc5ae2d4f7fa56fb2983d3e81921a35
benj-lazaro <benjie.work@gmail.com>
2026-01-28 20:26:22 +0800

    08 anonymous functions

---
+++ 06-more-on-functions/08-anonymous-functions/code/app.js
@@ -0,0 +1,11 @@
+const startGameBtn = document.getElementById("start-game-btn");
+
+// An Anonymous function as a defined in-place callback function
+startGameBtn.addEventListener("click", function () {
+  console.log("Game is starting...", age);
+});
+
+// Named Anonymous function as a defined in-place callback function
+// startGameBtn.addEventListener("click", function greet() {
+//   console.log("Game is starting...", age);
+// });
