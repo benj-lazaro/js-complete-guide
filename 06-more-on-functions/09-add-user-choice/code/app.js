f3775facbd50cf9ece55d7d4f73b1355bec08858
benj-lazaro <benjie.work@gmail.com>
2026-01-29 22:24:56 +0800

    09 working on the project adding user choices to the game

---
+++ 06-more-on-functions/09-add-user-choice/code/app.js
@@ -0,0 +1,36 @@
+const startGameBtn = document.getElementById("start-game-btn");
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}`,
+    "",
+  ).toUpperCase();
+
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We choose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+// Anonymous function defined as an in-place callback function
+startGameBtn.addEventListener("click", function () {
+  if (gameIsRunning) {
+    console.log(gameIsRunning);
+    return;
+  }
+
+  gameIsRunning = true;
+
+  console.log("Game is starting...");
+
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
+});
