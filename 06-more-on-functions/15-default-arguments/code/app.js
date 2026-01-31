7ada578df55693221384172392762aec03bddd84
benj-lazaro <benjie.work@gmail.com>
2026-01-31 22:20:42 +0800

    15 default arguments in functions

---
+++ 06-more-on-functions/15-default-arguments/code/app.js
@@ -0,0 +1,83 @@
+const startGameBtn = document.getElementById("start-game-btn");
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER";
+const RESULT_COMPUTER_WINS = "COMPUTER";
+
+let gameIsRunning = false;
+
+const getPlayerChoice = () => {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}`,
+    "",
+  ).toUpperCase();
+
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We choose ${DEFAULT_USER_CHOICE} for you!`);
+    return;
+  }
+
+  return selection;
+};
+
+const getComputerChoice = () => {
+  const randomValue = Math.random();
+
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+// Arrow function w/ a single-line ternary expression
+const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) =>
+  cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+        (cChoice === PAPER && pChoice === SCISSORS) ||
+        (cChoice === SCISSORS && pChoice === ROCK)
+      ? RESULT_PLAYER_WINS
+      : RESULT_COMPUTER_WINS;
+
+// Anonymous function defined as an in-place callback function
+startGameBtn.addEventListener("click", () => {
+  if (gameIsRunning) {
+    console.log(gameIsRunning);
+    return;
+  }
+
+  // Disables the "Start Game!" button
+  gameIsRunning = true;
+  console.log("Game is starting...");
+
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+
+  if (playerChoice) {
+    winner = getWinner(computerChoice, playerChoice);
+  } else {
+    winner = getWinner(computerChoice);
+  }
+
+  let message = `You picked ${playerChoice || DEFAULT_USER_CHOICE}, computer picked ${computerChoice}, therefore you `;
+
+  if (winner === RESULT_DRAW) {
+    message = message + "had a draw.";
+  } else if (winner === RESULT_PLAYER_WINS) {
+    message = message + "won.";
+  } else {
+    message = message + "lost.";
+  }
+
+  alert(message);
+
+  // Enables the "Start Game!" button
+  gameIsRunning = false;
+});
