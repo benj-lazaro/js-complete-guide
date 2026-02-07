bcfd5500831243e41705728475e3425b9f93ebd8
benj-lazaro <benjie.work@gmail.com>
2026-02-07 22:50:47 +0800

    09 exploring & changing dom properties

---
+++ 07-working-with-the-dom/09-explore-change-dom/code/app.js
@@ -0,0 +1,12 @@
+// Select the HTML element <h1>
+const h1 = document.querySelector("h1");
+
+// Change text content
+h1.textContent = "Some new text!";
+
+// Assign a class attribute
+h1.className = "title";
+
+// Change font & background colors
+h1.style.color = "white";
+h1.style.backgroundColor = "red";
