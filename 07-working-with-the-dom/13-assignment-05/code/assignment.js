777bead3d52ad5cc3ddc5eb6c86832ef281867cc
benj-lazaro <benjie.work@gmail.com>
2026-02-09 16:01:00 +0800

    13 assignment 05 time to practice dom querying

---
+++ 07-working-with-the-dom/13-assignment-05/code/assignment.js
@@ -0,0 +1,17 @@
+// Task #1
+const taskOneBackgroundColor = document.getElementById("task-1");
+taskOneBackgroundColor.style.backgroundColor = "black";
+
+const taskOneColor = document.querySelector("li");
+taskOneColor.style.color = "white";
+
+// Task #2
+// const taskTwo = document.querySelector("title");
+// taskTwo.textContent = "Assignment - Solved!";
+
+const taskTwo = document.querySelector("head title");
+taskTwo.textContent = "Assignment - Solved!";
+
+// Task #3
+const taskThree = document.querySelector("h1");
+taskThree.textContent = "Assignment - Solved!";
