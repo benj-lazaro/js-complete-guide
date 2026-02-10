9931358b9e6669940a1599c0bd49c5b59d40ce00
benj-lazaro <benjie.work@gmail.com>
2026-02-10 20:43:25 +0800

    17 selecting sibling elements

---
+++ 07-working-with-the-dom/17-select-sibling-elements/code/app.js
@@ -0,0 +1,17 @@
+const ul = document.querySelector("ul");
+
+// Select previous sibling Element node
+const previousElementSibling = ul.previousElementSibling;
+console.log(previousElementSibling);
+
+// Select previous sibling node
+const previousSibling = ul.previousSibling;
+console.log(previousSibling);
+
+// Select next sibling Element node
+const nextElementSibling = ul.nextElementSibling;
+console.log(nextElementSibling);
+
+// Select next sibling node
+const nextSibling = ul.nextSibling;
+console.log(nextSibling);
