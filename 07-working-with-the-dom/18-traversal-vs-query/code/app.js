96b434cff862415ab6c5cbe5b2d07b70c08e023c
benj-lazaro <benjie.work@gmail.com>
2026-02-10 21:34:43 +0800

    18 dom traversal vs query methods

---
+++ 07-working-with-the-dom/18-traversal-vs-query/code/app.js
@@ -0,0 +1,5 @@
+const ul = document.body.firstElementChild.nextElementSibling;
+const firstLi = ul.firstElementChild;
+
+// Returns a different Element node after the HTML code has been refactored
+console.log(firstLi);
