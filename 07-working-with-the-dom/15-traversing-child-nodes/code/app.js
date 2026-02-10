2d843025db4f68e5bb9e2ef6df0c77985916f486
benj-lazaro <benjie.work@gmail.com>
2026-02-10 17:32:34 +0800

    14 traversing child nodes

---
+++ 07-working-with-the-dom/15-traversing-child-nodes/code/app.js
@@ -0,0 +1,6 @@
+// Select the 2nd child of the HTML element <ul>
+const ul = document.querySelector("ul");
+
+// Use the property ".children" to access ALL child ELement nodes
+// Specifically select the 2nd child via its index value
+console.log(ul.children[1]);
