bc64658111012386ae7e75e59e7ff4a9b75e112e
benj-lazaro <benjie.work@gmail.com>
2026-02-13 19:09:50 +0800

    31 selecting the modal & "add" button

---
+++ 07-working-with-the-dom/31-select-modal-add-button/code/assets/scripts/app.js
@@ -0,0 +1,9 @@
+// Select the hidden modal
+const addMovieModal = document.getElementById("add-modal");
+
+// Alternative selction methods
+// const addMovieModal = document.querySelector('#add-modal');
+// const addMovieModal = document.body.children[1];
+
+// Select the button "ADD MOVIE"
+const startAddMovieButton = document.querySelector("header button");
