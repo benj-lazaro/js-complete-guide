9397322bb236b30e98cc778774ac9866853739ed
benj-lazaro <benjie.work@gmail.com>
2026-02-20 18:44:46 +0800

    11 is it included?

---
+++ 08-more-on-arrays-iterables/11-the-includes-method/code/app.js
@@ -0,0 +1,6 @@
+// Array of primitive values (numbers)
+const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
+
+// Searches for the existence of specific primitive values in the array
+console.log(testResults.includes(10.99));
+console.log(testResults.includes(47));
