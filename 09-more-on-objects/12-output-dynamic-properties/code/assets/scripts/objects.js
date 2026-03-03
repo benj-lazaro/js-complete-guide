9fe7c296ea2a80bb457e4ebf50864ccaea4ae258
benj-lazaro <benjie.work@gmail.com>
2026-03-03 18:11:22 +0800

    12 for-in loops & outputting dynamic properties

---
+++ 09-more-on-objects/12-output-dynamic-properties/code/assets/scripts/objects.js
@@ -0,0 +1,82 @@
+// Query DOM element node(S)
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
+
+// Store "newMovie" object(s)
+const movies = [];
+
+// Callback function(s)
+const renderMovie = () => {
+  // Parent HTML element <ul>
+  const movieList = document.getElementById("movie-list");
+
+  if (movies.length === 0) {
+    movieList.classList.remove("visible");
+    return;
+  } else {
+    movieList.classList.add("visible");
+  }
+
+  // This line is NOT an ideal implementation
+  movieList.innerHTML = "";
+
+  // Render each object as a child <li> of the parent HTML element <ul>
+  movies.forEach((movie, index, movies) => {
+    const movieElement = document.createElement("li");
+
+    // Fetch the property "title"
+    let text = movie.info.title + " - ";
+
+    // Iterate through the dynamic property
+    for (const key in movie.info) {
+      if (key !== "title") {
+        text = text + `${key}: ${movie.info[key]}`;
+      }
+    }
+
+    movieElement.textContent = text;
+    movieList.append(movieElement);
+  });
+};
+
+const clearUserInput = () => {
+  const userInputs = document.querySelectorAll(".control input");
+
+  for (const userInput of userInputs) {
+    userInput.value = "";
+  }
+};
+
+const addMovieHandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
+
+  // User input validation
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
+  }
+
+  // Create the object
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random(),
+  };
+
+  // Store the object
+  movies.push(newMovie);
+
+  clearUserInput();
+
+  renderMovie();
+};
+
+// Event handler(s)
+addMovieBtn.addEventListener("click", addMovieHandler);
