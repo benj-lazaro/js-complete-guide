b5261482afee602fd9225fa382c617b40d492d20
benj-lazaro <benjie.work@gmail.com>
2026-03-12 17:00:11 +0800

    10 communication can be challenging

---
+++ 10-classes-oop/10-class-communication/code/assets/scripts/app.js
@@ -0,0 +1,120 @@
+// Class that defines a product item
+class Product {
+  constructor(title, image, price, description) {
+    this.title = title;
+    this.imageUrl = image;
+    this.price = price;
+    this.description = description;
+  }
+}
+
+// Class that renders the Element node for the total amount of selected items
+class ShoppingCart {
+  items = [];
+
+  addProduct(product) {
+    this.items.push(product);
+    this.outputTotal = `<h2>Total: \$${1}</h2>`;
+  }
+
+  render() {
+    const cartEl = document.createElement("section");
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Ordler Now!</button>
+    `;
+
+    cartEl.className = "cart";
+
+    // Dynamically adds a new field
+    this.outputTotal = cartEl.querySelector("h2");
+
+    return cartEl;
+  }
+}
+
+// Class that renders the Element node for individual product item
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    console.log("Adding to cart");
+    console.log(this.product);
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+        <div>
+            <img src="${this.product.imageUrl}" alt="${this.product.title}">
+
+            <div class="product-item__content">
+                <h2>${this.product.title}</h2>
+                <h3>\$ ${this.product.price}</h3>
+                <p>${this.product.description}</p>
+                <button>Add to Cart</button>
+            </div>
+        </div>
+      `;
+
+    const addCartButton = prodEl.querySelector("button");
+    addCartButton.addEventListener("click", this.addToCart.bind(this));
+
+    return prodEl;
+  }
+}
+
+// Class that stores individual product items in an array
+class ProductList {
+  products = [
+    new Product(
+      "A Pillow",
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%2Fid%2FOIP.9UtdOxfFxHcVNKnkblTIBwHaHa%3Fpid%3DApi&f=1&ipt=8654694e58dc9093f53aa36c0b06801410e65dccbb6a013dbbbcb1e2c2f0d02c",
+      19.99,
+      "A soft pillow!",
+    ),
+    new Product(
+      "A Carpet",
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.teahub.io%2Fphotos%2Ffull%2F200-2004153_carpet.jpg&f=1&nofb=1&ipt=32f5c6713ceed3de746e30c05ed5e48ddf8e99f1fa57abb0a6f6a42b68d91928",
+      89.99,
+      "A carpet which you might like - or not.",
+    ),
+  ];
+
+  constructor() {}
+
+  render() {
+    const prodList = document.createElement("ul");
+
+    prodList.className = "product-list";
+
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
+      prodList.append(prodEl);
+    }
+    return prodList;
+  }
+}
+
+// Class that renders invidual parts of the shopping cart in the DOM
+class Shop {
+  render() {
+    const renderHook = document.getElementById("app");
+
+    const cart = new ShoppingCart();
+    const cartEl = cart.render();
+
+    const productList = new ProductList();
+    const prodListEl = productList.render();
+
+    renderHook.append(cartEl);
+    renderHook.append(prodListEl);
+  }
+}
+
+const shop = new Shop();
+shop.render();
