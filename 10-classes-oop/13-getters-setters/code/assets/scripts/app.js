b9dbda762ae81f892661f873f8ef3148bbf9cfe2
benj-lazaro <benjie.work@gmail.com>
2026-03-13 22:34:58 +0800

    13 getters & setters

---
+++ 10-classes-oop/13-getters-setters/code/assets/scripts/app.js
@@ -0,0 +1,160 @@
+// Class that defines a product item
+class Product {
+  constructor(title, image, price, description) {
+    this.title = title;
+    this.imageUrl = image;
+    this.price = price;
+    this.description = description;
+  }
+}
+
+// Class that renders the Element node for the total amount of selected items
+class ShoppingCart {
+  items = [];
+
+  // Setter
+  set cartItems(value) {
+    // Override existing array w/ a new one
+    this.items = value;
+
+    // Trigger the Getter to recalculate the value of "totalAmount"
+    this.outputTotal.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2>`;
+  }
+
+  // Getter
+  get totalAmount() {
+    const sum = this.items.reduce((previousValue, currentItem) => {
+      return previousValue + currentItem.price;
+    }, 0);
+
+    return sum;
+  }
+
+  addProduct(product) {
+    // Create a copy of the field "items"
+    const updatedItems = [...this.items];
+
+    // Add a new product in the cart
+    updatedItems.push(product);
+
+    // Trigger the Setter
+    this.cartItems = updatedItems;
+  }
+
+  render() {
+    const cartEl = document.createElement("section");
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Ordler Now!</button>
+    `;
+
+    cartEl.className = "cart";
+
+    // Dynamically adds a new field
+    this.outputTotal = cartEl.querySelector("h2");
+
+    return cartEl;
+  }
+}
+
+// Class that renders the Element node for individual product item
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    App.addProductToCart(this.product);
+
+    // For debugging ONLY
+    console.log(this.product);
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+        <div>
+            <img src="${this.product.imageUrl}" alt="${this.product.title}">
+
+            <div class="product-item__content">
+                <h2>${this.product.title}</h2>
+                <h3>\$ ${this.product.price}</h3>
+                <p>${this.product.description}</p>
+                <button>Add to Cart</button>
+            </div>
+        </div>
+      `;
+
+    const addCartButton = prodEl.querySelector("button");
+    addCartButton.addEventListener("click", this.addToCart.bind(this));
+
+    return prodEl;
+  }
+}
+
+// Class that stores individual product items in an array
+class ProductList {
+  products = [
+    new Product(
+      "A Pillow",
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%2Fid%2FOIP.9UtdOxfFxHcVNKnkblTIBwHaHa%3Fpid%3DApi&f=1&ipt=8654694e58dc9093f53aa36c0b06801410e65dccbb6a013dbbbcb1e2c2f0d02c",
+      19.99,
+      "A soft pillow!",
+    ),
+    new Product(
+      "A Carpet",
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.teahub.io%2Fphotos%2Ffull%2F200-2004153_carpet.jpg&f=1&nofb=1&ipt=32f5c6713ceed3de746e30c05ed5e48ddf8e99f1fa57abb0a6f6a42b68d91928",
+      89.99,
+      "A carpet which you might like - or not.",
+    ),
+  ];
+
+  constructor() {}
+
+  render() {
+    const prodList = document.createElement("ul");
+
+    prodList.className = "product-list";
+
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
+      prodList.append(prodEl);
+    }
+    return prodList;
+  }
+}
+
+// Class that renders invidual parts of the shopping cart in the DOM
+class Shop {
+  render() {
+    const renderHook = document.getElementById("app");
+
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
+
+    const productList = new ProductList();
+    const prodListEl = productList.render();
+
+    renderHook.append(cartEl);
+    renderHook.append(prodListEl);
+  }
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
