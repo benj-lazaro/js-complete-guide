0203c4b59ecf640687983478ca75852040472a73
benj-lazaro <benjie.work@gmail.com>
2026-03-15 18:03:16 +0800

    06 special key names & square bracket property access content & code refactor

--- 09-more-on-objects/06-square-bracket-property/code/assets/scripts/objects.js
+++ 09-more-on-objects/06-square-bracket-property/code/assets/scripts/objects.js
@@ -2,22 +2,23 @@
 const person = {
   "first name": "Max",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
   greet: function () {
     alert("Hello there!");
   },
-  "job-title": "Full-stack web developer",
 };
 
-// Access the key using the square bracket notation
-console.log(`First Name: ${person["first name"]}`);
-console.log(`Job Title: ${person["job-title"]}`);
+console.log(person);
 
-// Working w/ DOM Element nodes
+// Use Square bracket notation to access the special propery "first name"
+console.log(`First Name: ${person["first name"]}`);
+
+// Select the HTML element from the document w/ the id "movie-list"
 const movieList = document.getElementById("movie-list");
 console.log(movieList);
 
-// Access the Element node's "style" object
+// Access the Element node's property "style.display" & assign "block"
 movieList.style.display = "block";
 
-// Access the CSS property "background-color" using the square bracket notation
+// Access the Element node's property "background-color" using Square bracket notation
 movieList.style["background-color"] = "purple";
