8567cfdd3e8029634cb63df1db493053bf96ab70
benj-lazaro <benjie.work@gmail.com>
2026-03-18 22:43:12 +0800

    16 the object.assign() content & code refactor

--- 09-more-on-objects/16-object-assign/code/scripts.js
+++ 09-more-on-objects/16-object-assign/code/scripts.js
@@ -4,9 +4,13 @@ const person = {
   hobbies: ["cooking", "sports"],
 };
 
+// Copy source object "person" into a new object "person2"
 const person2 = Object.assign(person);
+
+// Push a new value to the property "hobbies" of the object "person"
 person.hobbies.push("pottery");
 
+// Copy source object "person" & add a new property "occupation" into a new object "person3"
 const person3 = Object.assign({ occupation: "assassin" }, person);
 
 console.log(person);
