624eb7ab1ef4f684f1c9228985329eeb2002adaa
benj-lazaro <benjie.work@gmail.com>
2026-03-19 16:40:53 +0800

    16 the object.assign() content & code refactor

--- 09-more-on-objects/16-object-assign/code/scripts.js
+++ 09-more-on-objects/16-object-assign/code/scripts.js
@@ -4,18 +4,23 @@ const person = {
   hobbies: ["cooking", "sports"],
 };
 
-// Copy source object "person" into a new object "person2"
-const person2 = Object.assign(person);
+// Perform a deep-copy of the object "person" into a new object "person2"
+const person2 = Object.assign({}, person);
 
-// Push a new value to the property "hobbies" of the object "person"
+// Update property "name" of the object "person"
+person.name = "Maximillian";
+
+// Then push a new value to the property "hobbies" of the object "person"
 person.hobbies.push("pottery");
 
-// Copy source object "person" & add a new property "occupation" into a new object "person3"
-const person3 = Object.assign({ occupation: "cook" }, person);
-
-// Contains identifical properties
+// Objects "person" & "person2" have different primitive values but identical reference values
 console.log(person);
 console.log(person2);
 
-// Contains an additional property "occupation"
+// Perform a deep-copy of the object "person"
+// Pre-define the property "occupation" assigned w/ a primitive value
+const person3 = Object.assign({ occupation: "cook" }, person);
+
+// ONLY the object "person3" has a property named "occupation" that is
+// NOT present in both objects "person" & "person2"
 console.log(person3);
