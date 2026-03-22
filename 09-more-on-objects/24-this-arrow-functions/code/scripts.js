492a7945b36a81dc26398984ca63c724160560ff
benj-lazaro <benjie.work@gmail.com>
2026-03-22 21:04:01 +0800

    24 this and the arrow functions code & content refactor

--- 09-more-on-objects/24-this-arrow-functions/code/scripts.js
+++ 09-more-on-objects/24-this-arrow-functions/code/scripts.js
@@ -1,6 +1,20 @@
 // The keyword "this" & the Arrow function (method)
-
 const members = {
+  teamName: "Blue Rockets",
+  people: ["Max", "Manuel"],
+  getTeamMembers() {
+    this.people.forEach((person) => {
+      console.log(this);
+      console.log(person + " - " + this.teamName);
+    });
+  },
+};
+
+members.getTeamMembers();
+console.log("=======");
+
+// The keyword "this" & the Anonymous function
+const members2 = {
   teamName: "Blue Rockets",
   people: ["Max", "Manuel"],
   getTeamMembers() {
@@ -11,4 +25,5 @@ const members = {
   },
 };
 
-members.getTeamMembers();
+members2.getTeamMembers();
+console.log("=======");
