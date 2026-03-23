2354a0f5c13c92b7e749fca3e58cbc9065c2d98a
benj-lazaro <benjie.work@gmail.com>
2026-03-23 22:38:32 +0800

    25 "this" summary code & content refactor

--- 09-more-on-objects/25-this-summary/code/scripts.js
+++ 09-more-on-objects/25-this-summary/code/scripts.js
@@ -1,19 +1,22 @@
-// #1 "this" in a global context
+// #1 "this" in a global context; returns a global Window object
 console.log(this);
+console.log("----------");
 
-// #2 "this" in a non-Arrow function, called in the global context
+// #2 "this" in a normal function, called in the global context & returns a global Window object
 function somethingOne() {
   console.log(this);
 }
 somethingOne();
+console.log("----------");
 
-// #3 "this" in an Arrow function, called in the global context
+// #3 "this" in an Arrow function, called in the global context & returns a global Window object
 const somethingTwo = () => {
   console.log(this);
 };
 somethingTwo();
+console.log("----------");
 
-// #4 "this" in a method (non-Arrow function), called on an object
+// #4 "this" in a method (non-Arrow function), called on an object; returns the value of the property "name"
 const personOne = {
   name: "Max",
   greet() {
@@ -21,6 +24,7 @@ const personOne = {
   },
 };
 personOne.greet();
+console.log("----------");
 
 // #5 "this" in a method (Arrow function), called on an object
 const personTwo = {
@@ -29,12 +33,12 @@ const personTwo = {
     console.log(this.name);
   },
 };
-personTwo.greet(); // "this" refers nothing or the global Window object
+personTwo.greet(); // "this" references nothing or the global Window object
 
 // #6 "this" call in on some other object
 const anotherPerson = {
   name: "Manuel",
 };
 
-anotherPerson.sayHi = personOne.greet; // Adds a new method to the object
-anotherPerson.sayHi(); // "this" refers to the thing w/c called it (i.e. anotherPerson)
+anotherPerson.sayHi = personOne.greet; // Adds a new method on-the-fly named "sayHi"; copies the "greet()" method of "personOne" object
+anotherPerson.sayHi(); // "this" references the object "anotherPerson" as it calls/executes the method "sayHi"
