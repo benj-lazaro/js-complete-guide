7fa4a15a8ddfafa1b6827fa773e76faffde2f4f1
benj-lazaro <benjie.work@gmail.com>
2026-04-23 21:09:16 +0800

    23 the instanceof operator

---
+++ 10-classes-oop/23-instanceof-operator/code/app.js
@@ -0,0 +1,10 @@
+class Person {
+  name = "Max";
+}
+
+// Instantiate Class Person into an object
+const person = new Person();
+console.log(person);
+
+// Check if object "person" is an instance of the Class "Person"
+console.log(person instanceof Person);
