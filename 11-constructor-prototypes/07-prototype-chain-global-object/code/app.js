b9a6da3c87347bc68db4b7635d6fc15076c30ce6
benj-lazaro <benjie.work@gmail.com>
2026-04-28 20:17:11 +0800

    07 the prototype chain & the global object

---
+++ 11-constructor-prototypes/07-prototype-chain-global-object/code/app.js
@@ -0,0 +1,36 @@
+// Constructor function
+function Person() {
+  this.name = "Max";
+  this.age = 40;
+  this.greet = function () {
+    console.log(`Hi I am ${this.name} and I am ${this.age}`);
+  };
+}
+
+// To preserve the property ".constructor" of the created object's "prototype object"
+Person.prototype.printAge = function () {
+  console.log(this.age);
+};
+
+// Adds a static function (method) to the Constructor function "Person"
+// Gets included in the property ".constructor" of the "prototype object"
+Person.describe = function () {
+  console.log("Creating a Person object...");
+};
+
+const person = new Person();
+console.log(person);
+console.log(person.__proto__);
+
+// Access method that is NOT defined in the created object's Constructor function
+console.log(person.toString());
+
+// Access static method of the Constructor function "Person"
+Person.describe();
+
+// View details of the Global Object Constructor function
+console.dir(Object);
+console.dir(Object.prototype);
+
+// The end of the Prototype Chain
+console.dir(Object.prototype.__proto__);
