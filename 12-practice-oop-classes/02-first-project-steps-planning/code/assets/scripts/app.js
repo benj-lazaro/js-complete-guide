715297c4ef122e3de8d863c78985e28081202c3b
benj-lazaro <benjie.work@gmail.com>
2026-05-05 16:04:42 +0800

    02 first project steps & planning

---
+++ 12-practice-oop-classes/02-first-project-steps-planning/code/assets/scripts/app.js
@@ -0,0 +1,11 @@
+// Class that handles the button "More Info" of a "ProjectItem" object
+class Tooltip {}
+
+// Class that represents a project item
+class ProjectItem {}
+
+// Class that handles multiple instances of "ProjectItem" objects
+class ProjectList {}
+
+// Class that manages the app overall
+class App {}
