3e0b0a307474d6c957759e929e2deb802c7cd0b7
benj-lazaro <benjie.work@gmail.com>
2026-06-04 19:27:59 +0800

    14 the error object & constructor function

--- 13-dom-browser-api/12-navigator-object/code/assets/scripts/analytics.js
+++
@@ -1,9 +0,0 @@
-// Executes the mock-up analytics every 2000ms
-const intervalId = setInterval(() => {
-  console.log("Sending analytics...");
-}, 2000);
-
-// Hookup an Event listener that halts the "setInterval()" execution
-document.getElementById("stop-analytics-btn").addEventListener("click", () => {
-  clearInterval(intervalId);
-});
