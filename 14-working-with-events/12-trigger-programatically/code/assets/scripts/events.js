465b2d9e516d3dcfdf01ff6f27b3eeae34ebdef9
benj-lazaro <benjie.work@gmail.com>
2026-06-15 20:51:37 +0800

    12 triggering dom elements programatically

---
+++ 14-working-with-events/12-trigger-programatically/code/assets/scripts/events.js
@@ -0,0 +1,36 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  // Prevents the default behavior of "submittin" the form data
+  event.preventDefault();
+
+  // Displays a "SubmitEvent" object on the JavaScript console
+  console.log(event);
+});
+
+const div = document.querySelector("div");
+div.addEventListener("click", (event) => {
+  console.log("Clicked <div>");
+  console.log(event);
+});
+
+const button = document.querySelector("button");
+button.addEventListener("click", (event) => {
+  event.stopPropagation();
+  console.log("Clicked <button>");
+  console.log(event);
+});
+
+const listItems = document.querySelectorAll("li");
+const list = document.querySelector("ul");
+
+list.addEventListener("click", (event) => {
+  event.target.closest("li").classList.toggle("highlight");
+
+  // Triggers a "form" submission programatically by simulated a form submission
+  // Skips the registered Event listner
+  // form.submit();
+
+  // Triggers a "form" submission programatically by simulated a mouse click
+  // Executes the registered Event listener
+  button.click();
+});
