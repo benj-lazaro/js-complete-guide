3684be5b5dcb8959872a8f1e93f613d0ba05d884
benj-lazaro <benjie.work@gmail.com>
2026-06-26 20:01:47 +0800

    05 the global number & math objects

---
+++ 16-numbers-and-strings/05-global-number-math-objects/code/app.js
@@ -0,0 +1,7 @@
+// Property "Infinity" & "-Infinity"
+console.log(Infinity);
+console.log(-Infinity);
+
+// Method ".isFinite()"
+console.log(Number.isFinite(Infinity)); // Returns "false"
+console.log(Number.isFinite(10)); // Returns "true"
