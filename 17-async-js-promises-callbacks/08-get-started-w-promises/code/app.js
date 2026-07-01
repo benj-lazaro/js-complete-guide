db0ca697a99b0d7edf765abe573e7f40b9658d7b
benj-lazaro <benjie.work@gmail.com>
2026-07-01 23:10:43 +0800

    08 getting started with promises

---
+++ 17-async-js-promises-callbacks/08-get-started-w-promises/code/app.js
@@ -0,0 +1,37 @@
+const button = document.querySelector("button");
+const output = document.querySelector("p");
+
+// Wraps a non-promise function & method w/in a "Promise" object
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Timeout Done!");
+    }, duration);
+  });
+
+  return promise;
+};
+
+// Callback function
+function trackUserHandler() {
+  // Offloads method to the browser
+  navigator.geolocation.getCurrentPosition(
+    (positionData) => {
+      setTimer(2000).then((data) => {
+        console.log(data, positionData);
+      });
+    },
+    (error) => console.log(error),
+  );
+
+  // Offloads the timeout to the browser w/ 0ms delay
+  setTimer(0).then((data) => {
+    console.log(data);
+  });
+
+  // Immediately executes BEFORE the ".getCurrentPosition()" & "setTimeout()" is done
+  console.log("Getting user position...");
+}
+
+// JavaScript offloads the Event listener & callback function to the browser's Event Loop
+button.addEventListener("click", trackUserHandler);
