5fb87886e7bf6b5c8d41cfdebdf66b91694fe35a
benj-lazaro <benjie.work@gmail.com>
2026-07-02 17:01:53 +0800

    08 getting started with promises code refactor

--- 17-async-js-promises-callbacks/08-get-started-w-promises/code/app.js
+++ 17-async-js-promises-callbacks/08-get-started-w-promises/code/app.js
@@ -1,7 +1,7 @@
 const button = document.querySelector("button");
 const output = document.querySelector("p");
 
-// Wraps a non-promise function & method w/in a "Promise" object
+// Wrap the non-promise function "setTimeout()" w/in a "Promise" object
 const setTimer = (duration) => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -14,7 +14,6 @@ const setTimer = (duration) => {
 
 // Callback function
 function trackUserHandler() {
-  // Offloads method to the browser
   navigator.geolocation.getCurrentPosition(
     (positionData) => {
       setTimer(2000).then((data) => {
@@ -24,12 +23,11 @@ function trackUserHandler() {
     (error) => console.log(error),
   );
 
-  // Offloads the timeout to the browser w/ 0ms delay
   setTimer(0).then((data) => {
     console.log(data);
   });
 
-  // Immediately executes BEFORE the two (2) promise calls
+  // Immediately executes BEFORE the "setTimer()" calls
   console.log("Getting user position...");
 }
 
