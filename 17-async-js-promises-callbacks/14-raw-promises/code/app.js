d5e3ca1f739ed00934a5b452023d4ae6c93f9acd
benj-lazaro <benjie.work@gmail.com>
2026-07-04 23:40:42 +0800

    14 async / await vs raw promises

---
+++ 17-async-js-promises-callbacks/14-raw-promises/code/app.js
@@ -0,0 +1,47 @@
+const button = document.querySelector("button");
+const output = document.querySelector("p");
+
+// Wrap the non-promise method ".getCurrentPosition()" w/in a "Promise" object
+const getPosition = (options) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        resolve(success); // Contains fetched position data
+      },
+      (error) => {
+        reject(error); // Contains error message
+      },
+      options,
+    );
+  });
+  return promise;
+};
+
+// Wrap the non-promise function "setTimeout()" w/in a "Promise" object
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Timeout Done!");
+    }, duration);
+  });
+  return promise;
+};
+
+// Callback function
+async function trackUserHandler() {
+  // Concise Promise Chaining
+  const positionData = await getPosition(); // Get geolocation position
+  const timerData = await setTimer(2000); // Wait for 2s
+
+  console.log(timerData, positionData);
+
+  // Waits for "await" to be resolved BEFORE executing the following lines of code
+  setTimer(1000).then(() => {
+    console.log("Timer done!");
+  });
+
+  console.log("Getting user position...");
+}
+
+// JavaScript offloads the Event listener & callback function to the browser's Event Loop
+button.addEventListener("click", trackUserHandler);
