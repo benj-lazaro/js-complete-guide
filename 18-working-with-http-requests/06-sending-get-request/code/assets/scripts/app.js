5337972f8450bc647e004b377629c598589a497e
benj-lazaro <benjie.work@gmail.com>
2026-07-15 22:58:27 +0800

    06 sending a get request

---
+++ 18-working-with-http-requests/06-sending-get-request/code/assets/scripts/app.js
@@ -0,0 +1,8 @@
+// Create a XMLHttpRequest object
+const xhr = new XMLHttpRequest();
+
+// Configure the XMLHttpRequest object
+xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+
+// Send the HTTP request
+xhr.send();
