d3701bc771f2a424e13bd0f6e21dcb657383850d
benj-lazaro <benjie.work@gmail.com>
2026-08-10 19:51:36 +0800

    16 using 3rd party packages

---
+++ 21-javascript-tooling-workflows/16-using-3rd-party-packages/code/webpack.config.prod.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/app.js",
+  output: {
+    filename: "[contenthash].js",
+    path: path.resolve(__dirname, "assets", "scripts"),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: "./",
+    },
+  },
+  //   devtool: [{ type: "javascript", use: "source-map" }],
+};
