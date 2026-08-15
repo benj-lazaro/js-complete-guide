50c3157db1feba20d39fa530d60128a319caf2e1
benj-lazaro <benjie.work@gmail.com>
2026-08-15 23:43:45 +0800

    06 getting started with indexeddb code & content refactor

--- 22-utilizing-browser-storage/06-indexeddb/code/app.js
+++ 22-utilizing-browser-storage/06-indexeddb/code/app.js
@@ -1,47 +1,47 @@
 const storeBtn = document.getElementById("store-btn");
 const retrieveBtn = document.getElementById("retrieve-btn");
 
+// Create a new database or open a connection to an existing one
+const dbRequest = indexedDB.open("StorageDummy", 1);
+
+// Database request successful
+dbRequest.onupgradeneeded = function (event) {
+  // Access the database
+  const db = event.target.result;
+
+  // Create an Object Store (i.e. table) & configure key needed to identify stored objects (i.e. items)
+  const objStore = db.createObjectStore("products", { keyPath: "id" });
+
+  // Configure Object Store's transaction mode
+  objStore.transaction.oncomplete = function (event) {
+    const productsStore = db
+      .transaction("products", "readwrite")
+      .objectStore("products");
+
+    // Add new item(s) to the Object Store w/in "StorageDummy"
+    productsStore.add({
+      id: "product1",
+      title: "A First Product",
+      price: 12.99,
+      tags: ["expensive", "luxury"],
+    });
+
+    productsStore.add({
+      id: "product2",
+      title: "A Second Product",
+      price: 42.99,
+      tags: ["more expensive", "insane luxury"],
+    });
+  };
+};
+
+// Database request failed
+dbRequest.onerror = function (event) {
+  console.log("Error!");
+};
+
 // Store data on IndexedDB
-storeBtn.addEventListener("click", () => {
-  // Establish a connection to the IndexedDB storage
-  const dbRequest = indexedDB.open("StorageDummy", 1);
-
-  // Event Handler when the database connection is successful
-  dbRequest.onupgradeneeded = function (event) {
-    const db = event.target.result;
-
-    // Create an Object Store (database table) & configure it
-    const objStore = db.createObjectStore("products", { keyPath: "id" });
-
-    // Initialize Object Store w/ individual items
-    objStore.transaction.oncomplete = function (event) {
-      // Configure the Object Store's transaction mode ("readonly" or "readwrite")
-      const productsStore = db
-        .transaction("products", "readwrite")
-        .objectStore("products");
-
-      // Add items items to the Object Store, each identified by their "keyPath" (i.e. "id")
-      productsStore.add({
-        id: "product1",
-        title: "A First Product",
-        price: 12.99,
-        tags: ["expensive", "luxury"],
-      });
-
-      productsStore.add({
-        id: "product2",
-        title: "A Second Product",
-        price: 42.99,
-        tags: ["more expensive", "insane luxury"],
-      });
-    };
-  };
-
-  // Event Handler when the database request fails
-  dbRequest.onerror = function (event) {
-    console.log("Error!");
-  };
-});
+storeBtn.addEventListener("click", () => {});
 
 // Retrieve data from IndexedDB
 retrieveBtn.addEventListener("click", () => {});
