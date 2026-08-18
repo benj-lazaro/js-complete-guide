314025f83f8b055778fbf04863d6145b13fe8b7a
benj-lazaro <benjie.work@gmail.com>
2026-08-18 18:14:59 +0800

    05 solution: feature detection & fallback code

---
+++ 23-browser-support/05-feature-detection/code/src/app.js
@@ -0,0 +1,26 @@
+// NOTE: Code is NOW supported by a broad variety of browsers
+// It was NOT back in 2019 when this lecture was published
+
+const button = document.querySelector("button");
+const textParagraph = document.querySelector("p");
+
+button.addEventListener("click", () => {
+  const text = textParagraph.textContent;
+
+  // Simple feature detection; returns "undefined" (falsy) if NOT supported
+  if (navigation) {
+    // Copies paragraph text to clipboard
+    navigator.clipboard
+      .writeText(text)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    console.log("Copied to clipboard...");
+  } else {
+    alert("Feature NOT available. Please copy manually.");
+  }
+});
